a592519f1cc7abc43df7ed5b2fba6a13f7ffbffc
Sam Ruby <rubys@intertwingly.net>
2022-09-20 00:30:59 -0400

    cleanup

--- lib/fly.io-rails/generators.rb
+++ lib/fly.io-rails/generators.rb
@@ -1,15 +1,3 @@
 require 'rails/generators'
 require 'rails/generators/channel/channel_generator'
 require 'rails/generators/job/job_generator'
-
-class Rails::Generators::ChannelGenerator
-  def configure_fly
-    STDERR.puts 'Configuring fly...'
-  end
-end
-
-class Rails::Generators::JobGenerator
-  def configure_fly
-    STDERR.puts 'Configuring fly...'
-  end
-end
--- lib/fly.io-rails/version.rb
+++ lib/fly.io-rails/version.rb
@@ -1,3 +1,3 @@
 module Fly_io
-  VERSION = '0.0.17'
+  VERSION = '0.0.18'
 end
